cc74d635a6ee6b1322588441bceca73f77220463
Karthik CM <karthikcm.77@gmail.com>
2021-08-08 20:43:42 -0700

    Portfolio Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello CMK!</h2>
+    </div>
+  );
+}
+
+export default App;
