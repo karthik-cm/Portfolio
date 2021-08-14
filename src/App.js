f7452f78f3133dccba48cb354b8b03cdfc82603e
Karthik CM <karthikcm.77@gmail.com>
2021-08-13 23:12:38 -0700

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ReactParticles from 'react-particles-js';
 import NavigationBar from './Components/NavigationBar';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <NavigationBar />
+    <React.Fragment>
+      <ReactParticles className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: '#f9ab00'
+              }
+            }
+          }
+        }}
+      />
+
+      <NavigationBar />
+      <Header />
+    </React.Fragment>
   );
 }
 
